78f0186ba004fe1d7daa6f8478ce365fc47f9b5b
Alex Geovanni <A180065@unach.mx>
2023-07-18 02:12:21 -0600

    Casi al 100%

--- src/scripts/jav.js
+++ src/scripts/jav.js
@@ -4,26 +4,39 @@
 // para abir el menu de navegacion en dispositivo movil
 const iconMenu = document.querySelector('header [data-menu ]'); 
 const Nav = document.querySelector('#nav-items');
-const item_A = document.querySelectorAll('#nav-items ul a')
+const item_A = document.querySelectorAll('#nav-items ul a');
 iconMenu.addEventListener('click',()=>{
     Nav.classList.toggle('show');
 })
 
-item_A.forEach((i)=>i.addEventListener('click',()=>Nav.classList.remove('show')))
+item_A.forEach((i)=>i.addEventListener('click',()=>Nav.classList.remove('show')));
 
 //  para los modos dark y light de la web
 
 const Body = document.querySelector('body');
 const dark = document.querySelector('#dark')
-const light = document.querySelector('#light')
-const Ul = document.querySelector('#nav-items ul')
+const light = document.querySelector('#light');
+const Ul = document.querySelector('#nav-items ul');
+const formSpan = document.querySelectorAll('form span');
+
 
 let isTrue = localStorage.getItem('isTrue');
         isTrue =JSON.parse(isTrue);
 
+
+if(isTrue == true){
+    Body.classList.add('dark-mod');
+    Ul.classList.add('dark-ul');
+    formSpan.forEach((i)=>i.classList.add('dark-span')); 
+
+    dark.style.display='none';
+    light.style.display='block';
+}
+
 light.addEventListener('click',()=>{
     Body.classList.toggle('dark-mod');
     Ul.classList.toggle('dark-ul');
+    formSpan.forEach((i)=>i.classList.toggle('dark-span'));; 
     localStorage.setItem('isTrue','false');
 
     light.style.display='none';
@@ -31,19 +44,15 @@ light.addEventListener('click',()=>{
 })
 
 dark.addEventListener('click',()=>{
-    Body.classList.toggle('dark-mod')
-    Ul.classList.toggle('dark-ul')    
-    localStorage.setItem('isTrue','true')
+    Body.classList.toggle('dark-mod');
+    Ul.classList.toggle('dark-ul');
+    formSpan.forEach((i)=>i.classList.toggle('dark-span'));  
+
+    localStorage.setItem('isTrue','true');
 
     dark.style.display='none';
     light.style.display='block';
 })
 
 
-if(isTrue == true){
-    Body.classList.toggle('dark-mod');
-    Ul.classList.toggle('dark-ul');
 
-    dark.style.display='none';
-    light.style.display='block';
-}
