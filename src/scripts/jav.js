9126aa112050e51890c117b13809dff996e6b019
Alex G <A180065@unach.mx>
2024-03-18 03:01:19 -0700

    agregando mi inicio en la progrmacion

--- src/scripts/jav.js
+++ src/scripts/jav.js
@@ -19,30 +19,40 @@ const light = document.querySelector('#light');
 const Ul = document.querySelector('#nav-items ul');
 const formSpan = document.querySelectorAll('form span');
 
-// Verifica si el dispositivo tiene activado el modo oscuro y almacena en el localStorage el boolean
-if(window.matchMedia){
-    localStorage.setItem('isTrue','true');
-    Body.classList.toggle('dark-mod');
-    Ul.classList.toggle('dark-ul');
-    formSpan.forEach((i)=>i.classList.toggle('dark-span'));  
-
-    dark.style.display='block';
-    light.style.display='none';
-} 
-
-
 // recupera lo almacenado de tipo boolean
 let isTrue = localStorage.getItem('isTrue');
         isTrue =JSON.parse(isTrue);
 
-if(isTrue == true){
+// Verifica si el dispositivo tiene activado el modo oscuro y almacena en el localStorage el boolean
+if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
     Body.classList.add('dark-mod');
     Ul.classList.add('dark-ul');
     formSpan.forEach((i)=>i.classList.add('dark-span')); 
 
     dark.style.display='none';
     light.style.display='block';
+}else{
+    Body.classList.remove('dark-mod');
+    Ul.classList.remove('dark-ul');
+    formSpan.forEach((i)=>i.classList.remove('dark-span'));; 
+    localStorage.setItem('isTrue','false');
+
+    light.style.display='none';
+    dark.style.display='block';
 }
+
+
+
+// if(isTrue == true){
+//     Body.classList.add('dark-mod');
+//     Ul.classList.add('dark-ul');
+//     formSpan.forEach((i)=>i.classList.add('dark-span')); 
+
+//     dark.style.display='none';
+//     light.style.display='block';
+// }
+
+// icons para cambiar modo dark o light
 
 light.addEventListener('click',()=>{
     Body.classList.toggle('dark-mod');
